f1460274292d76e594eaa7364164470bd649b5e3
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-10-06 00:56:47 +0530

    Initial Commit

---
+++ Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Course
+//
+//  Created by Dalveer singh on 06/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Course/CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseApp.swift
+//  Course
+//
+//  Created by Dalveer singh on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
