adaaa8f4f91a56b31fa0113e908e9aae12e6c6b9
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-10-09 23:31:14 +0530

    Card design completed

--- Course/ContentView.swift
+++ Course/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 import RiveRuntime
 struct ContentView: View {
+    @AppStorage("selectedTab") var selectedTab:Tab = .chat
+    @State var isOpen:Bool = false
+    let button = RiveViewModel(fileName: "menu_button",stateMachineName: "State Machine",autoPlay: false)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            switch selectedTab {
+            case .chat:
+                Text("chat")
+            case .search:
+                Text("Search")
+            case .timer:
+                Text("Timer")
+            case .bell:
+                Text("Bell")
+            case .user:
+                Text("User")
+            }
+            button.view()
+                .frame(width:44,height:44)
+                .mask(Circle())
+                .frame(maxWidth:.infinity,maxHeight: .infinity,alignment: .topLeading)
+                .padding()
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .onTapGesture {
+                    button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
+            TabBar()
+        }
     }
 }
 
---
+++ Course/Views/VCard.swift
@@ -0,0 +1,56 @@
+//
+//  VCard.swift
+//  Course
+//
+//  Created by Dalveer singh on 09/10/22.
+//
+
+import SwiftUI
+
+struct VCard: View {
+    var course:Course
+    var body: some View {
+        VStack(alignment:.leading,spacing:8){
+            Text(course.title)
+                .customFont(.title2)
+                .layoutPriority(1)
+                .frame(maxWidth:170,alignment: .leading)
+            Text(course.subtitle)
+                .opacity(0.7)
+                .customFont(.subheadline)
+                .frame(maxWidth:.infinity,alignment:.leading)
+            Text(course.caption.uppercased())
+                .customFont(.footnote2)
+            Spacer()
+            HStack {
+                ForEach(Array([4,5,6].shuffled().enumerated()),id:\.offset) { index,number in
+                    Image("Avatar \(number)")
+                        .resizable()
+                        .frame(width:44,height:44)
+                    .mask(Circle())
+                    .offset(x:CGFloat(index * -20))
+                }
+            }
+        }
+        .foregroundColor(.white)
+        .padding(30)
+        .frame(width:260,height: 310)
+        .background(.linearGradient(colors: [course.color,course.color.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+        .mask (
+            RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: course.color.opacity(0.3), radius: 8, x: 0, y: 12)
+        .shadow(color: course.color.opacity(0.3), radius: 2, x: 0, y: 1)
+        .overlay(
+            course.image
+                .frame(maxWidth:.infinity,maxHeight: .infinity,alignment: .topTrailing)
+                .padding(20)
+        )
+        
+    }
+}
+
+struct VCard_Previews: PreviewProvider {
+    static var previews: some View {
+        VCard(course: courses[0])
+    }
+}
